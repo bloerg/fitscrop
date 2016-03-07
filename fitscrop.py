45b6d856511a462a1f8d8f8266a73fcc6c39ab5d
Blörg <devel@bloerg.de>
2016-03-07 13:13:02 +0100

    check for existence of output path

--- fitscrop.py
+++ fitscrop.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 import argparse
 from astropy.io import fits
 ## or
@@ -15,7 +16,7 @@ if __name__ == '__main__':
     #input parameter parsing
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--inputfile", type=str, required=True, help="Path to a FITS file containing an image.")
-    parser.add_argument("-o", "--outputdir", type=str, required=True, help="Path to a FITS file which will contain the cropped image.")
+    parser.add_argument("-o", "--outputfile", type=str, required=True, help="Path to a FITS file which will contain the cropped image.")
     parser.add_argument("-H", "--hdu", type=int, default=0, help="Number of the Header Data Unit containing the image within the input file.")
     parser.add_argument("-c", "--coordinate_format", type=str, default="pixel", help="Format of the cropping coordinates (-l, -r, -t -b). One of {pixel, degree}.")
     parser.add_argument("-l", "--left", type=float, required=True, help="Left boundary coordinate for cropping.")
@@ -24,11 +25,18 @@ if __name__ == '__main__':
     parser.add_argument("-b", "--bottom", type=float, required=True, help="Bottom boundary coordinate for cropping.")
     args = parser.parse_args()    
 
+    ## Check for output directory existence
+    if not os.path.exists(os.path.dirname(args.outputfile)):
+        sys.stderr.write("Error: output directory does not exist. Please create.\n")
+    else:
+        try:
+            ## open input fits file
+            f = fits.open(args.inputfile)
+            
+            ## close input fits file
+            f.close()
+        except:
+            sys.stderr.write("Error: cannot open input file.\n")
     
-    try:
-        f = fits.open(args.inputfile)
-    except:
-        sys.stderr.write("Error: cannot open input file.\n")
+    
 
-    
-    f.close()
